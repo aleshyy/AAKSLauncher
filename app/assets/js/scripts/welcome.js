9f22b8c682f31e28186fa12aad680476a51b37aa
ale <ale.shy@icloud.com>
2025-01-08 03:34:51 -0300

    Somes fixes, update background images and bump version to 2.0.3

--- app/assets/js/scripts/welcome.js
+++ app/assets/js/scripts/welcome.js
@@ -2,7 +2,7 @@
  * Script for welcome.ejs
  */
 document.getElementById('welcomeButton').addEventListener('click', e => {
-    loginOptionsCancelEnabled(true) // False by default, be explicit.
+    loginOptionsCancelEnabled(false) // False by default, be explicit.
     loginOptionsViewOnLoginSuccess = VIEWS.landing
     loginOptionsViewOnLoginCancel = VIEWS.loginOptions
     switchView(VIEWS.welcome, VIEWS.loginOptions)
